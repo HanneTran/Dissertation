645c932c6a7d4e2c26687547153850db89e5073e
HanneTran <htran2@sheffield.ac.uk>
2019-02-19 17:13:55 +0100

    created question controller with index file

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,9 @@
+class QuestionController < ApplicationController
+  def index
+    @question = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'question/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #resources :patients
+  resources :patients
+  resources :questions
   #root to: :controller => "patient"
   root 'patient#index'
+
 #get 'patient/index'
 end
