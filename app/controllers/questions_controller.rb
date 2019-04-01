8d7036dcf74d93cb1907ee961ea3f3aa209e5d62
hannetran <htran2@sheffield.ac.uk>
2019-04-01 22:11:53 +0100

    patient and question connection working now

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -23,7 +23,16 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id]) # video of the specific q should be here
     @patient = Question.where(patient_id: params[:id])
-    @q1= Q1.find_by(patient_id: params[:id])
+    #@q= Q1.find_by(patient_id: params[:patient_id])
     @vid1= Q1.find_by(patient_id: params[:id])
+    print "HEEEEEEELLLLOOOOOOOOOOO  PARAMS[:ID] PRINT BELOW"
+    print params[:id]
+    if (params[:id] == '1')
+      @q = Q1.find_by(patient_id: params[:patient_id])
+      print "YAAAAAS QUESTION 1 "
+    else (params[:id] == '2')
+      print "NOOOOOOOOOOOO question 1"
+    end
+    #if question_id = 1 use table Q1.
   end 
 end
